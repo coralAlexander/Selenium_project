95f5a449ac40b685b24b29dd34ca2d2a26b50192
Alexander Coral <coral.alexander@gmail.com>
2021-07-01 20:52:25 +0300

    remove temp

--- src/main/java/temp/common/Config.java
+++
@@ -1,8 +0,0 @@
-package temp.common;
-
-public class Config {
-
-    public static final String BROWSER_AND_PLATFORM = "CHROME_WINDOWS";
-    public static final Boolean CLEAR_COOKIES = true;
-    public static final Boolean HOLD_BROWSER_OPEN = false;
-}
